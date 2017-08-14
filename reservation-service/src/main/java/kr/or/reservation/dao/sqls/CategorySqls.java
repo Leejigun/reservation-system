24b8b30ce869fb90815b5494306c730dd76b8f6d
YongWoo Jeon <i.nevalose@gmail.com>
2017-08-14 21:23:37 +0900

    Revert "D_missile [복습] 예약 메인페이지 + 예약페이지 [전용우, 한홍택] (#100)"
    This reverts commit 66c6437100663b2ddff8d6ca0e7f8256324f6d16.

--- reservation-service/src/main/java/kr/or/reservation/dao/sqls/CategorySqls.java
+++
@@ -1,6 +0,0 @@
-package kr.or.reservation.dao.sqls;
-
-public class CategorySqls {
-	public static final String SELECT_LIST = "SELECT * FROM category";
-	public static final String DELETE = "DELETE FROM category WHERE id = :id";
-}
